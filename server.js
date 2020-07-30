1db6d7a4fe22eb4b98f8c6e0799e005df31e7c6a
Ryan DiSalle <ryandisalle@yahoo.com>
2020-07-30 15:46:46 -0400

    final_Heroku

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -15,7 +16,6 @@ app.use(cors())
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-    console.log('validate bearer token middleware')
     //if no authToken or authToken split does not equal apiToken
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request' })
@@ -62,8 +62,18 @@ function handleGetPokemon(req, res) {
 //pokemon function request
 app.get('/pokemon', handleGetPokemon)
 
-const PORT = 8000
+// 4 parameters in middleware, express knows to treat this as error handler
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+  
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
 })
